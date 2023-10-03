49ac3ef8b0dd48ea99494a46f5533af59e3a844e
Mohammad Nehal Sayib <sayibnehal21@gmail.com>
2023-10-03 08:10:39 +0530

    Initial Project Setup

---
+++ src/test/java/com/mns/blogApp/BlogAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mns.blogApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
